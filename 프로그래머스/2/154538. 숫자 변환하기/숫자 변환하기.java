263db939b46410ffeed34a5eb0134c2fc6ec5f94
Subbni <87067033+subbni@users.noreply.github.com>
2024-06-05 10:07:46 +0900

    [level 2] Title: 숫자 변환하기, Time: 18.35 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -2,27 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int x, int y, int n) {
-        int answer = -1;
-        Queue<int[]> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean[y+1];
-        queue.add(new int[] {x,0});
-        while(!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            visited[cur[0]] = true;
-            if(cur[0] == y) {
-                answer = cur[1];
-                break;
+        int[] dp = new int[y+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        dp[x] = 0;
+        for(int i=x; i<=y; i++) {
+            if(dp[i] == Integer.MAX_VALUE) continue;
+            // 아직 도달하지 않은 숫자는 건너뛴다.
+            if(i * 2 <= y) {
+                dp[i*2] = Math.min(dp[i*2],dp[i]+1);
             }
-            if(cur[0]*2 <= y && !visited[cur[0]*2]) {
-                queue.add(new int[] {cur[0]*2, cur[1]+1});
+            if(i * 3 <= y) {
+                dp[i*3] = Math.min(dp[i*3],dp[i]+1);
             }
-            if(cur[0]*3 <= y && !visited[cur[0]*3]) {
-                queue.add(new int[] {cur[0]*3, cur[1]+1});
-            }
-            if(cur[0]+n <= y && !visited[cur[0]+n]) {
-                queue.add(new int[] {cur[0]+n, cur[1]+1});
+            if(i + n <= y) {
+                dp[i+n] = Math.min(dp[i+n],dp[i]+1);
             }
         }
-        return answer;
+        return dp[y] == Integer.MAX_VALUE ? -1 : dp[y];
     }
 }
