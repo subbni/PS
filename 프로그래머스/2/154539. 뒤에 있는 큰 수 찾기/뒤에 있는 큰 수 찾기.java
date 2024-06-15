32d8c9f5a2db8baa41392c02896d75be84a2e9da
Subbni <87067033+subbni@users.noreply.github.com>
2024-06-15 13:18:47 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 10.67 ms, Memory: 185 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length]; 
+        answer[numbers.length-1] = -1;
+        int max = numbers[numbers.length-1];
+        
+        for(int i=numbers.length-2; i>=0; i--) {
+            for(int j=i+1; j<numbers.length; j++) {
+                if(numbers[j] > numbers[i]) {
+                    answer[i] = numbers[j];
+                    break;
+                } else if(answer[j] > numbers[i]) {
+                    answer[i] = answer[j];
+                    break;
+                } else if(answer[j] == -1) {
+                    answer[i] = -1;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
