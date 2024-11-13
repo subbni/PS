d07d6f3661a01dcfbded6484d299171b81f72728
오자두 <87067033+subbni@users.noreply.github.com>
2024-11-13 22:02:16 +0900

    [Gold V] Title: 토마토, Time: 596 ms, Memory: 134384 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,53 +1,53 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] dy = {-1,1,0,0};
-    static int[] dx = {0,0,-1,1};
-
-    static int[][] tomato;
-    static int M,N;
-    static int leftTomato=0;
-    public static void main (String[] args) {
-        Scanner in = new Scanner(System.in);
-        // M = 가로(열의 수), N = 세로(행의 수)
-        int M = in.nextInt();
-        int N = in.nextInt();
-        tomato = new int[N][M];
-        int count = 0;
-        Queue<int []> q = new LinkedList<>();
-
-        for(int n=0;n<N;n++) {
-            for(int m=0;m<M;m++) {
-                tomato[n][m] = in.nextInt();
-                if(tomato[n][m]==0) leftTomato++; 
-                else if(tomato[n][m]==1) q.add(new int[]{n,m});
-            }
+  static int[] dx = {0,0,-1,1};
+  static int[] dy = {1,-1,0,0};
+  static int N,M;
+  static int[][] tomato;
+  static int leftTomato;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    M = Integer.parseInt(st.nextToken()); // 가로
+    N = Integer.parseInt(st.nextToken()); // 세로
+    tomato = new int[N][M];
+    Queue<int[][]> queue = new ArrayDeque<>();
+    for(int i=0; i<N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for(int j=0; j<M; j++) {
+        tomato[i][j] = Integer.parseInt(st.nextToken());
+        if(tomato[i][j]==0) {
+          leftTomato++;
+        } else if(tomato[i][j]==1) {
+          queue.add(new int[][] {{i,j,0}});
         }
- 
-        
-        while(!q.isEmpty() && leftTomato!=0) {
-            count++;
-            int size = q.size();
-            for(int i=0;i<size;i++) {
-                int[] cur = q.poll();
-
-                for(int j=0;j<4;j++) {
-                    int ny = cur[0]+dy[j];
-                    int nx = cur[1]+dx[j];
-    
-                    if(ny<0 || nx<0 || ny>=N || nx>=M || tomato[ny][nx]!=0) continue;
-
-                    tomato[ny][nx]=1;
-                    q.add(new int[]{ny,nx});
-                    leftTomato--;
-                }
-            }
-        }
-
-        System.out.println(leftTomato==0? count:-1);
-        in.close();
-        
+      }
     }
+    int answer = leftTomato == 0 ? 0 : getMinimumDays(queue);
+    System.out.println(answer);
+  }
+
+  public static int getMinimumDays(Queue<int[][]> queue) {
+    while(!queue.isEmpty()) {
+      int[][] cur = queue.poll();
+      int x = cur[0][0];
+      int y = cur[0][1];
+      int days = cur[0][2];
+      for(int i=0; i<4; i++) {
+        int nx = x+dx[i];
+        int ny = y+dy[i];
+        if(nx>=N || ny>=M || nx<0 || ny<0 || tomato[nx][ny]!=0) continue;
+        if(--leftTomato == 0) {
+          return days+1;
+        } else {
+          tomato[nx][ny] = 1;
+          queue.add(new int[][] {{nx,ny,days+1}});
+        }
+      }
+    }
+    return -1;
+  }
+
 }
