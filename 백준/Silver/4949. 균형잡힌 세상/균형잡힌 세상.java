36ed1e860b22b429c17430bea8e47aa4375ada3d
오자두 <87067033+subbni@users.noreply.github.com>
2024-11-13 20:37:48 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 180 ms, Memory: 19012 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,40 +1,41 @@
 import java.io.*;
-import java.util.*;
+import java.util.Stack;
 
 public class Main {
   public static void main(String[] args) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringBuffer sb = new StringBuffer();
     String sentence;
-    
-    while((sentence = br.readLine()) != null && !sentence.equals(".")) {
+    while(( sentence = br.readLine())!=null && !sentence.equals(".")) {
       Stack<Character> stack = new Stack<>();
-      boolean isBalanced = true;
-      
-      for (int i = 0; i < sentence.length(); i++) {
+      int i=0;
+      for(; i<sentence.length(); i++) {
         char c = sentence.charAt(i);
-        if (c == '(' || c == '[') {
-          stack.push(c);
-        } else if (c == ')' || c == ']') {
-          if (stack.isEmpty() || !checkPairBracket(stack.pop(), c)) {
-            isBalanced = false;
+        if(c == '(' || c=='[') {
+          stack.add(c);
+        } else if(c==')' || c==']') {
+          if(stack.isEmpty() || !checkPairBracket(stack.pop(), c)) {
             break;
           }
         }
       }
-      
-      if (isBalanced && stack.isEmpty()) {
+      if(i==sentence.length() && stack.isEmpty()) {
         sb.append("yes\n");
       } else {
         sb.append("no\n");
       }
     }
-    
     System.out.print(sb);
     br.close();
   }
 
-  public static boolean checkPairBracket(char open, char close) {
-    return (open == '(' && close == ')') || (open == '[' && close == ']');
+  public static boolean checkPairBracket(char first, char second) {
+    if(first == '(') {
+      return second == ')';
+    } else if(first == '[') {
+      return second == ']';
+    } else {
+      throw new IllegalArgumentException();
+    }
   }
 }
