15e6af1c11efbda13a6e2f383e961a49c3c3b07a
오자두 <87067033+subbni@users.noreply.github.com>
2024-11-14 12:09:40 +0900

    [Silver III] Title: N과 M (5), Time: 272 ms, Memory: 29116 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,53 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-    static int N;
-    static int M;
-    static boolean[] visited;
-    static int[] arr;
-    static int[] ans;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());  // N개의 자연수 중에서
-        M = Integer.parseInt(st.nextToken());  // M개를 고른 수열을 구하라.
-        arr = new int[N];
-        ans = new int[M];
-        visited = new boolean[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr);
-        
-        dfs(0);
-        
-        System.out.println(sb);
+  static int N,M;
+  static StringBuilder sb;
+  static int[] arr;
+  static int[] comb;
+  static boolean visited[];
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    arr = new int[N];
+    comb = new int[M];
+    visited = new boolean[N];
+    st = new StringTokenizer(br.readLine());
+    for(int i=0; i<N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
+    // 모든 수를 오름차순 정렬
+    Arrays.sort(arr); 
+    sb = new StringBuilder();
+    func(0,0);
+    System.out.print(sb);
+    br.close();
+  }
 
-    public static void dfs(int depth) {
-        if(depth == M) {
-            for(int val: ans) {
-                sb.append(val).append(' ');
-            }
-            sb.append('\n');
-            return;
-        }
-
-        for(int i=0; i<N; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                ans[depth] = arr[i];
-                dfs(depth+1);
-                visited[i] = false;
-            }
-        }
-        
+  public static void func(int cur, int idx) {
+    if(idx == M) {
+      for (int num : comb)  {
+        sb.append(num).append(' ');
+      }
+      sb.append("\n");
+      return;
     }
+    for(int i=0; i<N; i++) {
+      if(!visited[i]) {
+        comb[idx] = arr[i];
+        visited[i] = true;
+        func(i, idx+1);
+        visited[i] = false;
+      }
+    }
+  }
 }
