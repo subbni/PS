5ef67826713591aa3cc3a213b110c96fb80bc0e7
오자두 <87067033+subbni@users.noreply.github.com>
2024-11-14 11:52:25 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 16668 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -3,42 +3,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/*
- * 아래 조건을 만족하는 길이가 M인 수열을 모두 구하라.
- * 1부터 N까지 자연수 중에서 M개를 고른 수열
- * 같은 수를 여러 번 골라도 된다. 
- * 고른 수열은 비내림차순이어야 한다.
- * 길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
- */
 
 public class Main {
-    static int N;
-    static int M;
-    static int[] arr;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); 
-        M = Integer.parseInt(st.nextToken()); 
-        arr = new int[M];
+  static int N,M;
+  static StringBuilder sb;
+  static int[] comb;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    sb = new StringBuilder();
+    comb = new int[M];
+    func(0,0);
+    System.out.print(sb);
+    br.close();
+  }
 
-        dfs(1,0);
-        System.out.println(sb);
+  public static void func(int cur, int idx) {
+    if(idx == M) {
+      for (int num : comb)  {
+        sb.append(num).append(' ');
+      }
+      sb.append("\n");
+      return;
     }
-
-    public static void dfs(int pos, int depth) {
-        if(depth == M) {
-            for(int val: arr) {
-                sb.append(val).append(' ');
-            }
-            sb.append('\n');
-            return;
-        }
-
-        for(int i=pos; i<=N; i++) {
-            arr[depth] = i;
-            dfs(i,depth+1);
-        }
+    for(int i=cur; i<N; i++) {
+      comb[idx] = i+1;
+      func(i, idx+1);
     }
+  }
 }
