c16792bba028c1010cab1cf210341554e0f3c60b
오자두 <87067033+subbni@users.noreply.github.com>
2024-11-14 11:47:09 +0900

    [Silver III] Title: N과 M (3), Time: 408 ms, Memory: 68232 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -3,40 +3,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/*
- * 아래 조건을 만족하는 길이가 M인 수열을 모두 구하라.
- * 1부터 N까지 자연수 중에서 M개를 고른 수열
- * 같은 수를 여러 번 골라도 된다. 
- */
-
 public class Main {
-    static int N;
-    static int M;
-    static int[] arr;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); 
-        M = Integer.parseInt(st.nextToken()); 
-        arr = new int[M];
+  static int N,M;
+  static StringBuilder sb;
+  static int[] comb;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    sb = new StringBuilder();
+    comb = new int[M];
+    func(0);
+    System.out.print(sb);
+    br.close();
+  }
 
-        dfs(0);
-        System.out.println(sb);
+  public static void func(int idx) {
+    if(idx == M) {
+      for (int num : comb)  {
+        sb.append(num).append(' ');
+      }
+      sb.append("\n");
+      return;
     }
-
-    public static void dfs(int depth) {
-        if(depth == M) {
-            for(int val: arr) {
-                sb.append(val).append(' ');
-            }
-            sb.append('\n');
-            return;
-        }
-
-        for(int i=0; i<N; i++) {
-            arr[depth] = i+1;
-            dfs(depth+1);
-        }
+    for(int i=0; i<N; i++) {
+      comb[idx] = i+1;
+      func(idx+1);
     }
+  }
 }
