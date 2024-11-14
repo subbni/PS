75df33ebfdc853d151fc9c7518264084d54e0628
오자두 <87067033+subbni@users.noreply.github.com>
2024-11-14 12:16:03 +0900

    [Silver III] Title: N과 M (7), Time: 580 ms, Memory: 123788 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -5,43 +5,39 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
-    static int[] arr;
-    static int[] ans;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());  // N개의 자연수 중에서
-        M = Integer.parseInt(st.nextToken());  // M개를 고른 수열을 구하라.
-        arr = new int[N];
-        ans = new int[M];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr);
-        
-        dfs(0);
-        
-        System.out.println(sb);
+  static int N,M;
+  static int[] comb;
+  static int[] arr;
+  static StringBuilder sb;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    arr = new int[N];
+    comb = new int[M];
+    st = new StringTokenizer(br.readLine());
+    for(int i=0; i<N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
+    Arrays.sort(arr);
+    sb = new StringBuilder();
+    func(0);
+    System.out.print(sb);
+    br.close();
+  }
 
-    public static void dfs(int depth) {
-        if(depth == M) {
-            for(int val: ans) {
-                sb.append(val).append(' ');
-            }
-            sb.append('\n');
-            return;
-        }
-
-        for(int i=0; i<N; i++) {
-            ans[depth] = arr[i];
-            dfs(depth+1);
-        }
-        
+  public static void func(int idx) {
+    if(idx == M) {
+      for (int num : comb)  {
+        sb.append(num).append(' ');
+      }
+      sb.append("\n");
+      return;
     }
+    for(int i=0; i<N; i++) {
+      comb[idx] = arr[i];
+      func(idx+1);
+    }
+  }
 }
