389d3ae58899d278ca169ffea88131309bb41395
오자두 <87067033+subbni@users.noreply.github.com>
2024-11-14 14:41:55 +0900

    [Silver II] Title: 부분수열의 합, Time: 116 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,43 +1,38 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N,S;
-    static int[] arr;
-    static int cnt = 0;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-        arr = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        dfs(0,0);
-        if(S == 0) {
-            cnt -= 1;
-        } // sum이 0으로 초기화 되어 있기 때문에, dfs()시 아무것도 택하지 않는 경우 cnt++; 된다.
-        bw.write(cnt+"");
-        bw.flush();
-        bw.close();
-        br.close();
+  static int N, S;
+  static int[] arr;
+  static int cnt = 0;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    S = Integer.parseInt(st.nextToken());
+    st = new StringTokenizer(br.readLine());
+    arr = new int[N];
+    for(int i=0; i<N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
+    func(0,0);
+    if(S == 0) {
+      cnt--; // 공집합인 경우 제외
+    }
+    System.out.println(cnt);
+    br.close();
+  }
 
-    static void dfs(int depth, int sum) {
-        if(depth == N) {
-            if(sum == S) {
-                cnt++;
-            }
-            return;
-        }
-        dfs(depth+1,sum+arr[depth]);
-        dfs(depth+1, sum);
+  public static void func(int idx, int sum) {
+    if(idx == N) {
+      if(sum == S) {
+        cnt++;
+      }
+      return;
     }
+    func(idx+1, sum + arr[idx]); // idx번째 수를 부분수열에 포함
+    func(idx+1, sum); // idx번째 수를 부분수열에 포함 X
+  }
 }
