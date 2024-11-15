fcf118dd1b8e538961642d9159051fc640a39422
오자두 <87067033+subbni@users.noreply.github.com>
2024-11-15 19:17:13 +0900

    [Silver I] Title: RGB거리, Time: 124 ms, Memory: 22564 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -4,36 +4,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] cost; 
-    static int[][] memo;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        cost = new int[N+1][3]; // 0-빨, 1-초, 2-파
-        memo = new int[N+1][3]; 
-        StringTokenizer st;
-        for(int i=1; i<=N; i++) {
-            st = new StringTokenizer(br.readLine());
-            cost[i][0] = Integer.parseInt(st.nextToken());
-            cost[i][1] = Integer.parseInt(st.nextToken());
-            cost[i][2] = Integer.parseInt(st.nextToken());
-        }
-        memo[1][0] = cost[1][0];
-        memo[1][1] = cost[1][1];
-        memo[1][2] = cost[1][2];
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i<3; i++) {
-            dp(N,i);
-            if(min > memo[N][i]) min = memo[N][i];
-        }
-        System.out.println(min);
-    }
-
-    static void dp(int idx, int color) {
-        if(memo[idx][color]!=0) return;
-
-        dp(idx-1,(color+1)%3);
-        dp(idx-1,(color+2)%3);
-        memo[idx][color] = cost[idx][color] + Math.min(memo[idx-1][(color+1)%3],memo[idx-1][(color+2)%3]);
-    }
+	static int N;
+	static int[][] cost;
+	static int[][] d;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		cost= new int[N][N];
+		d = new int[N][N];
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			cost[i][0] = Integer.parseInt(st.nextToken());
+			cost[i][1] = Integer.parseInt(st.nextToken());
+			cost[i][2] = Integer.parseInt(st.nextToken());
+		}
+		d[0][0] = cost[0][0];
+		d[0][1] = cost[0][1];
+		d[0][2] = cost[0][2];
+		
+		int min = Math.min(dp(N-1,0),Math.min(dp(N-1,1),dp(N-1,2)));
+		System.out.print(min);
+	}
+	
+	public static int dp(int n, int color) {
+		if(n >= N) return Integer.MAX_VALUE;
+		if(d[n][color]!=0) return d[n][color];
+		return d[n][color] = Math.min(dp(n-1, (color+1)%3), dp(n-1, (color+2)%3))+cost[n][color];
+	}
 }
