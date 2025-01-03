b6dca3754e01822e6712adee261636dc2c9aa41c
오자두 <87067033+subbni@users.noreply.github.com>
2025-01-03 09:51:44 +0900

    [level 1] Title: 공원 산책, Time: 0.99 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] solution(String[] park, String[] routes) {
+    boolean[][] map = new boolean[park.length][park[0].length()]; 
+    int curX = 0; int curY = 0;
+    for(int i=0; i<park.length; i++) {
+        char[] rowArr = park[i].toCharArray();
+        for(int j=0; j<park[0].length(); j++) {
+            switch(rowArr[j]) {
+                case 'S'-> {
+                    map[i][j] = true;
+                    curX = i; curY = j;
+                }
+                case 'O' -> map[i][j] = true;
+                case 'X' -> map[i][j] = false;
+            }
+        }
+    }
+    
+    for(String route: routes) {
+        String[] command = route.split(" ");
+        boolean possibleCommand = true;        
+        int nx = curX;
+        int ny = curY;
+        for(int i=1; i<=Integer.parseInt(command[1]); i++) {
+          switch (command[0].charAt(0)) {
+            case 'N' -> nx--;
+            case 'E' -> ny++;
+            case 'S' -> nx++;
+            case 'W' -> ny--;
+          }
+          if(nx<0 || nx>=park.length || ny<0 || ny>=park[0].length() || !map[nx][ny]) {
+            possibleCommand = false;
+            break;
+          } 
+        }
+        if(possibleCommand) {
+          curX = nx;
+          curY = ny;
+        }
+      }
+    return new int[] {curX, curY};
+  }
+}
