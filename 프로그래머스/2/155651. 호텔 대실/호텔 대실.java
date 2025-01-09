948a6763a3f32f0b324cfd8353724072b53dabe0
오자두 <87067033+subbni@users.noreply.github.com>
2025-01-09 10:39:17 +0900

    [level 2] Title: 호텔 대실, Time: 12.68 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+     static public int solution(String[][] book_time) {
+    // PriorityQueue : 각 방의 퇴실 시간이 빠른 순대로 정렬
+    // 각 Stack : 퇴실 시간 + 10 저장
+    PriorityQueue<Stack<Integer>> roomQueue = new PriorityQueue<>((q1,q2)-> {
+      return q1.peek()-q2.peek();
+    });
+    Arrays.sort(book_time, (b1,b2)-> b1[0].compareTo(b2[0]));
+
+    for(int i=0; i<book_time.length;i++) {
+      int arriveMinutes = convertTimeToMinutes(book_time[i][0]);
+      int leaveMinutes = convertTimeToMinutes(book_time[i][1]);
+  
+      if(roomQueue.size()>0 && roomQueue.peek().peek() <= arriveMinutes) {
+        // 남는 방 존재
+        Stack<Integer> availableRoom = roomQueue.poll();
+        availableRoom.add(leaveMinutes+10);
+        roomQueue.add(availableRoom);
+      } else {
+        // 남는 or 사용한 방 없음
+        Stack<Integer> newRoom = new Stack<>();
+        newRoom.add(leaveMinutes+10);
+        roomQueue.add(newRoom);
+      }
+    }
+    return roomQueue.size();
+  }
+
+  static public int convertTimeToMinutes(String time) {
+    // time 형식: HH:MM
+    String[] timeTokens = time.split(":");
+    int result = Integer.parseInt(timeTokens[0])*60 + Integer.parseInt(timeTokens[1]);
+    return result;
+  }
+}
