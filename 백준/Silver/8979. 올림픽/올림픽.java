ba269c5e3c8de7a6e99be2f8c41bce42799a5dc4
오자두 <87067033+subbni@users.noreply.github.com>
2025-06-19 13:52:15 +0900

    [Silver V] Title: 올림픽, Time: 112 ms, Memory: 14272 KB, Score: 20 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -12,9 +13,21 @@ import java.util.StringTokenizer;
 // 출력 :
 // 국가 K의 등수
 
-// 풀이 : "금+은+동" 개수 String 사전순 반대로 Comparator 사용하기
+// 풀이 : 금-은-동 개수로 정렬하는 Comparator 적용한 PriorityQueue 사용
 
 class Main {
+    static class Nation {
+        int nation;
+        int gold;
+        int silver;
+        int bronze;
+        public Nation(int nation, int gold, int silver, int bronze) {
+            this.nation = nation;
+            this.gold = gold;
+            this.silver = silver;
+            this.bronze = bronze;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -22,30 +35,46 @@ class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        String[] medalStrings = new String[N+1];
-        PriorityQueue<String> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Nation> pq = new PriorityQueue<>((n1, n2) -> {
+            if (n1.gold == n2.gold) {
+                if (n1.silver == n2.silver) {
+                    return n2.bronze - n1.bronze;
+                } else {
+                    return n2.silver - n1.silver;
+                }
+            } else {
+                return n2.gold - n1.gold;
+            }
+        });
     
-        
         for (int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             int nation = Integer.parseInt(st.nextToken());
-            String medalString = "";
-            for (int j=0; j<3; j++) {
-                medalString += st.nextToken();
-            }
-            medalStrings[nation] = medalString;
-            pq.add(medalString);
+            int gold = Integer.parseInt(st.nextToken());
+            int silver = Integer.parseInt(st.nextToken());
+            int bronze = Integer.parseInt(st.nextToken());
+            pq.add(new Nation(nation, gold, silver, bronze));
         }
 
+        Nation previous = null;
         int rank = 1;
         while (!pq.isEmpty()) {
-            String medalString = pq.remove();
-            if (medalString.equals(medalStrings[K])) {
+            Nation current = pq.remove();
+
+            if (previous != null) {
+                if ((previous.gold != current.gold) || (previous.silver != current.silver) || (previous.bronze != current.bronze)) {
+                    rank++;
+                }
+            }
+
+            if (current.nation == K) {
                 break;
             }
-            rank++;
+
+            previous = current;
         }
 
         System.out.println(rank);
     }
+
 }
