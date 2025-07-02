5c52222e56c185fa10e53635a442314c948c82fd
오자두 <87067033+subbni@users.noreply.github.com>
2025-07-02 11:37:10 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 입력 : 낮에 올라갈 수 있는 A미터, 밤에 미끄러지는 B미터, 나무 막대 높이 V미터
+// 출력 : 달팽이가 나무 막대 정상에 가려면 며칠이 걸리는 지
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        
+        int answer = (V-A) / (A-B);
+        if ((V-A) % (A-B) != 0) answer++;
+
+        System.out.println(answer+1);
+    }
+}
